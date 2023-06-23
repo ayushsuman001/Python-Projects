87d8bb66173d4679e3353af44a761b191750ce8d
ayushsuman001 <126647036+ayushsuman001@users.noreply.github.com>
2023-06-23 23:58:45 +0530

    To-Do list final.py

--- To-Do list final.py
+++ To-Do list final.py
@@ -1,18 +1,14 @@
 import tkinter as tk
 
-# Create a new window
 window = tk.Tk()
 window.title("To-Do List Application")
 
-# Create a label widget for the title
 title_label = tk.Label(window, text="To-Do List", font=("Arial", 18))
 title_label.pack()
 
-# Create a text box widget for entering tasks
 task_entry = tk.Entry(window, width=50)
 task_entry.pack()
 
-# Create a list box widget for displaying tasks
 task_list = tk.Listbox(window, width=50)
 task_list.pack()
 
@@ -23,18 +19,14 @@ def add_task():
         task_list.insert(tk.END, task)
         task_entry.delete(0, tk.END)
 
-# Create a button widget for adding tasks
 add_button = tk.Button(window, text="Add Task", command=add_task)
 add_button.pack()
 
-# Define a function for removing tasks from the list
 def remove_task():
     selected_task = task_list.curselection()
     task_list.delete(selected_task)
 
-# Create a button widget for removing tasks
 remove_button = tk.Button(window, text="Remove Task", command=remove_task)
 remove_button.pack()
 
-# Start the main event loop
 window.mainloop()
